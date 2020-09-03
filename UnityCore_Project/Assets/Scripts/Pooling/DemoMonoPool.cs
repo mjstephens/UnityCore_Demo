af2588a70c0242c5d19d041445d01ed1886ac917
Michael Stephens <mstephens.dev@gmail.com>
2020-09-03 12:19:09 -0400

    Started pooling demo scene

---
+++ UnityCore_Project/Assets/Scripts/Pooling/DemoMonoPool.cs
@@ -0,0 +1,40 @@
+﻿using SharpCore.Utility.Pooling;
+using UnityCore.Instantiation;
+using UnityEngine;
+
+public class DemoMonoPool : MonoBehaviour
+{
+    [Header("References")]
+    public Transform pooledInstanceSpawnTrans;
+    public GameObject pooledInstancePrefab;
+
+    [Header("Pool Options")] 
+    public int poolCapacityMin;
+    public int poolCapacityMax;
+
+    private IPool _pool;
+
+    #region Initialization
+
+    private void Awake()
+    {
+        Instantiator.SetObjectPoolCapacity(pooledInstancePrefab, poolCapacityMin, poolCapacityMax);
+    }
+
+    #endregion Initialization
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnPooledInstance();
+        }
+    }
+
+    private void SpawnPooledInstance()
+    {
+        Instantiator.Pooled(pooledInstancePrefab, pooledInstanceSpawnTrans.position, Quaternion.identity);
+    }
+}
